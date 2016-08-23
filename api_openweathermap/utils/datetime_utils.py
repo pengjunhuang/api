5426c5d31b5cec394657dad342ca5fa739f22292
pjhuang <huangpj1110@gmail.com>
2016-08-23 03:17:10 -0700

    hardcoded tz, revised cron

--- api_openweathermap/utils/datetime_utils.py
+++ api_openweathermap/utils/datetime_utils.py
@@ -1,9 +1,11 @@
 import datetime
+import pytz
 
 def time_converter(time, format = '%b %d, %I:%M %p'):
     '''
      convert time int to specified date time format
     :return: human readable date time style
     '''
-    converted_time = datetime.datetime.fromtimestamp(int(time)).strftime(format)
+    # TODO: remove Hardcoded Timezone
+    converted_time = datetime.datetime.fromtimestamp(int(time), pytz.timezone('America/Los_Angeles')).strftime(format)
     return converted_time
